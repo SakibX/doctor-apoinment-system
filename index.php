68fbb6d5bc20add30df3e04d4c9fcb5eb1e0ccc6
Sajjadur Rahman Sakib <102736598+SakibX@users.noreply.github.com>
2024-02-23 20:41:07 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,494 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>We Care BD </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-heartbeat"></i> <strong>WeCare</strong>Medical</a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#services">services</a>
+       
+        
+        <a href="#blogs">blogs</a>
+        <a href="index3.php">Account</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="image">
+        <img src="image/Why-Choose-us.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>We take care of your healthy life</h3>
+        <p> A person who has good physical health is likely to have bodily functions and processes working at their peak.</p>
+        
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- icons section starts  -->
+
+<section class="icons-container">
+
+    <div class="icons">
+        <i class="fas fa-user-md"></i>
+        <h3>10</h3>
+        <p>doctors at work</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-users"></i>
+        <h3>150</h3>
+        <p>satisfied patients</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-procedures"></i>
+        <h3>100+</h3>
+        <p>bed facility</p>
+    </div>
+
+   
+
+</section>
+
+<!-- icons section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/about-img.svg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>take the world's best quality treatment</h3>
+            <p>Welcome to WeCare Hospital, where compassionate care meets cutting-edge expertise. At WeCare, our commitment is to your well-being, offering a comprehensive array of services tailored to meet your healthcare needs. From emergency care to specialized treatments, diagnostics to rehabilitative services, our dedicated team ensures personalized attention at every step of your journey.</p>
+           
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+    <h1 class="heading"> our <span>services</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-notes-medical"></i>
+            <h3>free checkups</h3>
+            <p> Preventive screenings, consultations, wellness assessments, no-cost diagnostic evaluations.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-ambulance"></i>
+            <h3>24/7 ambulance</h3>
+            <p> Immediate response, round-the-clock emergency transportation, skilled paramedics, rapid medical assistance.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-user-md"></i>
+            <h3>expert doctors</h3>
+            <p> Specialized physicians, experienced healthcare professionals, board-certified specialists, personalized treatments.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-pills"></i>
+            <h3>medicines</h3>
+            <p>Comprehensive pharmacy, quality medications, prescription fulfillment, diverse pharmaceutical options.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-procedures"></i>
+            <h3>bed facility</h3>
+            <p> Comfortable accommodations, state-of-the-art rooms, specialized care units, supportive and healing environments.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-heartbeat"></i>
+            <h3>total care</h3>
+            <p>Holistic approach, comprehensive health solutions, patient-centric focus, all-encompassing medical attention.</p>
+            <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- services section ends -->
+
+
+
+<!-- doctors section starts  -->
+
+<!-- <section class="doctors" id="doctors">
+
+    <h1 class="heading"> our <span>doctors</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/doc-01.jpg" alt="">
+            <h3>dr. Jahidur Rahman</h3>
+            <span>Neurologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+                
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-02.jpg" alt="">
+            <h3> Dr. Anowarul karim Chaowdhury</h3>
+            <span>Pediatrician</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-03.jpg" alt="">
+            <h3>Dr. Shamima jahid preeti</h3>
+            <span>Gynecologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-4.jpg" alt="">
+            <h3>Dr. Sharmin Jahan</h3>
+            <span>Psychiatrist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-5.jpg" alt="">
+            <h3>Dr. Abul hasan</h3>
+            <span>Dermatologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-6.jpg" alt="">
+            <h3>Dr. Kamrul Islam</h3>
+            <span>Gastroenterologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            
+            </div>
+        </div>
+        <div class="box">
+            <img src="image/doc-7.jpg" alt="">
+            <h3>DR Wajid Chowdhury</h3>
+            <span>intern doctor</span>
+            <div class="share">
+                
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="image/doc-8.jpg" alt="">
+            <h3>Dr. Sajid Hasan</h3>
+            <span>Oncologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="image/doc-9.jpg" alt="">
+            <h3>Dr. Hamim Uddin</h3>
+            <span>Hematologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section> -->
+
+<!-- doctors section ends -->
+
+<!-- appointmenting section starts   -->
+
+<!-- <section class="appointment" id="appointment">
+
+    <h1 class="heading"> <span>appointment</span> now </h1>    
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/appointment-img.svg" alt="">
+        </div>
+
+        <form action="
+      
+            <h3>make appointment</h3>
+            <input type="text"name="name" placeholder="your name" class="box">
+            <input type="number"name="number" placeholder="your number" class="box">
+            <input type="email"name="email" placeholder="your email" class="box">
+            <input type="date"name="date" class="box">
+            <input type="submit" name="submit" value="appointment now" class="btn">
+        </form>
+
+    </div>
+
+</section> -->
+
+<!-- appointmenting section ends -->
+
+<!-- review section starts  -->
+
+<!--
+<section class="review" id="review">
+    
+    <h1 class="heading"> client's <span>review</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/review-01.jpg" alt="">
+            <h3>MD. Sakib</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">"Choosing WeCare Hospital was a life-changing decision. The staff's empathy and professionalism were exceptional, making my treatment feel more like a supportive journey than a medical procedure."
+
+            </p>
+        </div>
+
+        <div class="box">
+            <img src="image/Review-02.jpg" alt="">
+            <h3>Shawon</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">"WeCare Hospital surpassed all my expectations. From the moment I arrived, the 24/7 ambulance service ensured immediate attention. The expert doctors took the time to explain everything, instilling confidence and trust."</p>
+        </div>
+
+        <div class="box">
+            <img src="image/review-03.jpg" alt="">
+            <h3>Mahi</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">"My experience at WeCare Hospital was nothing short of exceptional. The free checkups they offer are a testament to their commitment to community health. The medicines provided were readily available, easing my worries."</p>
+        </div>
+
+    </div>
+
+</section> -->
+
+<!-- review section ends -->
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> our <span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-1.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i>  01 January, 2023 </a>
+                    <a href="#"> <i class="fas fa-user"></i> by MUHI </a>
+                </div>
+                <h3> "Navigating Excellence: The Expertise Behind WeCare Hospital"</h3>
+                <p>Explore the core of WeCare Hospital's exceptional care, delving into the expertise of our distinguished doctors, their specialties, and the cutting-edge techniques</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 30 november, 2022 </a>
+                    <a href="#"> <i class="fas fa-user"></i> by SHAFAYET </a>
+                </div>
+                <h3>"Comfort Redefined: A Closer Look at Our Bed Facilities"</h3>
+                <p>Unveil the essence of comfort at WeCare Hospital. Peek into our state-of-the-art rooms, specialized care units, and the holistic approach to creating healing environments. </p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-3.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a href="#"> <i class="fas fa-calendar"></i> 01 DECEMBER, 2023 </a>
+                    <a href="#"> <i class="fas fa-user"></i> by SAJJAD </a>
+                </div>
+                <h3>"Total Wellness: Our Approach to Comprehensive Healthcare"</h3>
+                <p>Dive into the holistic healthcare philosophy at WeCare Hospital. Understand our commitment to total care</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#about"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> services </a>
+            <a href="#doctors"> <i class="fas fa-chevron-right"></i> doctors </a>
+            <a href="#appointment"> <i class="fas fa-chevron-right"></i> appointment </a>
+            <a href="#review"> <i class="fas fa-chevron-right"></i> review </a>
+            <a href="#blogs"> <i class="fas fa-chevron-right"></i> blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>our services</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i>
+                 Checkups </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i>Expert Doctors </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i>Medicins </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> diagnosis </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> ambulance service </a>
+        </div>
+
+        <div class="box">
+            <h3>appointment info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +8801704530260 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +8801842769913 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> Sajid9@gmail.com </a>
+            <a href="#"> <i class="fas fa-envelope"></i> Nasif26@gmail.com </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Patuakhali, Bangladesh </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="#"> <i class="fab fa-pinterest"></i> pinterest </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Suaib Bin Humayun </span> | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+<!-- js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
+
